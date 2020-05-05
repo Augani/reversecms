03b461ac82a66fd22dec4daada1fadfe558c0206
Augustus Otu <Augani7@gmail.com>
2020-05-05 15:18:16 +0000

    added build folder

---
+++ client/build/precache-manifest.46263b8d02180df051f0cce21ad29753.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee9787ee49d557ed2700c4473422a4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffba7acdacf5fa2922cb",
+    "url": "/static/css/main.4e7929d2.chunk.css"
+  },
+  {
+    "revision": "e4823c2894d2eb1b2ba0",
+    "url": "/static/js/2.b4e0b69a.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/static/js/2.b4e0b69a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffba7acdacf5fa2922cb",
+    "url": "/static/js/main.8b32c98c.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  },
+  {
+    "revision": "3e6ef6046d96073a96b955212e7d5906",
+    "url": "/static/media/apple-computer-decor-design-326502.3e6ef604.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
